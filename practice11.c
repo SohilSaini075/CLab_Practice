6ad882c86aed8614b407cfcd7d065266c0c87137
SohilSaini75 <sohilsaini075@gmail.com>
2025-09-11 16:17:22 +0530

    practice ques

---
+++ practice11.c
@@ -0,0 +1,26 @@
+/*
+1 
+2 2 
+3 3 3 
+4 4 4 4
+5 5 5 5 5    
+WAP to print this pattern
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j, n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
