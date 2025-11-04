356f7f804d7387d5804b1b72cda5c358648e0f4e
SohilSaini75 <sohilsaini075@gmail.com>
2025-11-04 14:35:27 +0530

    LabEXP_1 Ques

---
+++ Lab Exp_1/Ques2.c
@@ -0,0 +1,14 @@
+// Write a C Program to print your address in multiple lines (new line).
+
+#include <stdio.h>
+
+int main() {
+    printf("Name: Sohil Saini\n");
+    printf("House No: 123, Green Street\n");
+    printf("City: Panchkula\n");
+    printf("State: Haryana\n");
+    printf("Pin Code: 134116\n");
+    
+    return 0;
+}
+
